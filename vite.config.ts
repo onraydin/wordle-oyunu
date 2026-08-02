b9fb458e2f7fc1e63857194090558763b64141cc
onraydin <onur196318@gmail.com>
2026-08-02 04:11:24 +0300

    live-github

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wordle-oyunu/', // Buraya GitHub repository ismini tam olarak bu formatta yazmalısın
 })
